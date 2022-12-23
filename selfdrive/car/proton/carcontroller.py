565a94e5fb6e4517a98243bf655939464b259ca6
iXcess <brandonting@hotmail.com>
2022-12-23 18:59:04 +0800

    [MADS] Fix autoresume during full engage

--- selfdrive/car/proton/carcontroller.py
+++ selfdrive/car/proton/carcontroller.py
@@ -88,7 +88,7 @@ class CarController():
 
     if CS.out.standstill and enabled and (frame % 50 == 0):
       # Spam resume button to resume from standstill at max freq of 10 Hz.
-      if not self.mads:
+      if not self.mads or CS.acc_req:
         can_sends.append(send_buttons(self.packer, frame % 16, False))
 
     self.last_steer = apply_steer
